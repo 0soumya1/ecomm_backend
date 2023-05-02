19ad04094ba90de131e8b4753e43668cfbc8c23a
Soumya <soumyaguptabond@gamil.com>
2023-05-02 16:07:52 +0530

    live url set

--- db/config.js
+++ db/config.js
@@ -1,2 +1,4 @@
 const mongoose= require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
+const url = "mongodb+srv://soumyagupta:soumya12@ecomm.uvhbi0q.mongodb.net/?retryWrites=true&w=majority"
+const url2 = "mongodb://127.0.0.1:27017/e-commerce"
+mongoose.connect(url);
